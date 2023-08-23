35771cd9456bb12f7750ee4a422b835cda3d3a97
Joseph Chai <josephchai100@hotmail.com>
2023-08-24 00:08:13 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+sudo /home/ys_rk3568/toilet-feedback-backend/scripts/initCOM.sh
 chromium-browser --kiosk --noerdialogs --enable-features=OverlayScrollbar --password-store=basic --disable-web-security --user-data-dir="~/temp" --high-dpi-support=1 --force-device-scale-factor=1 --disable-pinch ~/tablet-ui-lottie-4-locations/html/home.html
 
 # /usr/bin/chromium-browser --kiosk --noerrdialogs --enable-features=OverlayScrollbar --disable-restore-session-state http://localhost:1988 --high-dpi-support=1 --force-device-scale-factor=1 --disable-pinch
